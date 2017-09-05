957d9207c2338cd3b911f87609807ad443bd2f11
Eugene <31668190+Blkzer0@users.noreply.github.com>
2017-09-05 16:57:39 -0400

    Add files via upload

---
+++ iso2usb.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+
+##script needs to run in sudo to copy to usb
+
+if [[ $EUID -ne 0 ]]; then
+	echo "ERROR! Run this script with root user!"
+	exit 1
+fi
+
+lsblk
+
+   echo "What is the usb path?:"
+   read path
+   echo "What is the iso path?:"
+   read iso
+   dd bs=1M if=$iso of=$path
+   sleep 1 
+   echo "iso has been written to usb"
+
+## (bs) block size can be set to 4M for faster copy but may 
+## not work so its set to default 1M
